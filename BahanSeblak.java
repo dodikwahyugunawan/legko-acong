0014a85b9087a27674675ca7f01eaf645383cc79
dodikwahyugunawan <hasbiyallah212@gmail.com>
2022-10-05 21:06:51 -0700

    resep baru

---
+++ BahanSeblak.java
@@ -0,0 +1,7 @@
+public class BahanSeblak {
+    public static void main(String[] args) {
+        System.out.println("Resep seblak enak");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Cari di google banyak males ngetik banyak bahanya soalnya");
+    }
+}
