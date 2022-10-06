36244c10e605510441dd1a2eb16838fa3dc8e740
dodikwahyugunawan <hasbiyallah212@gmail.com>
2022-10-05 21:02:05 -0700

    tambahan resep rahasia acong

---
+++ ResepLengko.java
@@ -0,0 +1,13 @@
+public class ResepLengko {
+public static void main(String[] args) {
+    
+
+System.out.println("resep Lengko Acong");
+System.out.println("----------------------------------------------------------------");
+System.out.println("1. kecap");
+System.out.println("2. bumbu");
+System.out.println("3. timun");
+System.out.println("4. oreg");
+System.out.println("5. tabahan lauk (Opsional)");
+}
+}
